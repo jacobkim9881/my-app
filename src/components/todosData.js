0a55b0a54955484f38b579d8a30ba2afcd1bd979
root <jacobkim@gmail.com>
2019-09-10 14:06:47 +0900

    commit-msg

---
+++ src/components/todosData.js
@@ -0,0 +1,22 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true,
+    },
+    
+    {
+        id: 2,
+        text: "Eat lunch with whom",
+        completed: true,
+    },
+
+    {
+        id: 3,
+        text: "Sleep",
+        completed: false,
+    }
+    
+]
+
+export default todosData
