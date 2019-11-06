4ac73d9a8f032886aa77ad85441c7f6a6ca60311
kim <jacobkim@gmail.com>
2019-11-06 20:27:20 +0900

    commit-msg

---
+++ src/components/header/Head.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Head extends React.Component {
+    render() {
+        return (
+            <div>
+                <div style={{
+                    height: "5rem",
+                    fontSize: "2rem",
+                    textAlign: "center"
+                }}>
+                    My Cafe
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Head;
